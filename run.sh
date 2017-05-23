ebedb47c575dc9980826c77a4516d3d95a617141
Niels Højen <niels@hojen.net>
2017-05-23 14:05:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-VERSION=$1
-
 yum -y install wget unzip
-wget https://fwdl.filewave.com/$VERSION/FileWave_Linux_$VERSION.zip
-unzip FileWave_Linux_$VERSION.zip
-rm -f fwxserver-$VERSION-1.0.x86_64.rpm
-yum install -y --nogpgcheck fwbooster-$VERSION-1.0.x86_64.rpm
+wget https://fwdl.filewave.com/$FWBOOSTER_VERSION/FileWave_Linux_$FWBOOSTER_VERSION.zip
+unzip FileWave_Linux_$FWBOOSTER_VERSION.zip
+rm -f fwxserver-$FWBOOSTER_VERSION-1.0.x86_64.rpm
+yum install -y --nogpgcheck fwbooster-$FWBOOSTER_VERSION-1.0.x86_64.rpm
