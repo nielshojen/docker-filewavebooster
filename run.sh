0572eae4e4957d1744ed8d9547acc9ef43bbe377
Niels Højen <niels@hojen.net>
2017-05-29 00:42:42 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yum -y install wget unzip
-wget https://fwdl.filewave.com/$FWBOOSTER_VERSION/FileWave_Linux_$FWBOOSTER_VERSION.zip
-unzip FileWave_Linux_$FWBOOSTER_VERSION.zip
-rm -f fwxserver-$FWBOOSTER_VERSION-1.0.x86_64.rpm
-yum install -y --nogpgcheck fwbooster-$FWBOOSTER_VERSION-1.0.x86_64.rpm
