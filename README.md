3bd7c7a2f684b1382e9865e09243ebf9450f7cc3
Niels Højen <niels.hojen@bestseller.com>
2020-04-29 12:30:15 +0200

    adding more settings

--- README.md
+++ README.md
@@ -13,10 +13,16 @@ Several options are customizable using environment variables.
 * ``FWBOOSTER_PASSWORD``: The password of the Booster. Defaults to ``filewave``.
 * ``FWBOOSTER_PORT``: The TCP port of the Booster. Defaults to ``20013``.
 * ``FWBOOSTER_PUBPORT``: The Publish of the Booster. Defaults to ``20003``.
-* ``FWSERVER1_PORT``: The TCP Port of Server 1 that the Booster Connects to. Defaults to ``20015``.
-* ``FWSERVER1_ADDR``: The IP or DNS address of Server 1 that the Booster Connects to. Defaults to ``no.server.set``.
-* ``FWSERVER1_SUBSPORT``: The Subscriptions TCP port of Server 1 that the Booster Connects to. Defaults to ``20005``.
-* ``FWBOOSTER_SSLLOADER``: Boolean value to turn SSL on/off for Loader Connections. Defaults to ``0``.
+* ``FWBOOSTER_FWSERVERPORT``: The TCP Port of the FileWave Server. Defaults to ``20015``.
+* ``FWBOOSTER_FWSERVERADDR``: The IP or DNS address of the FileWave Server. Defaults to ``no.server.set``.
+* ``FWBOOSTER_SERVER1PORT``: The TCP Port of the Upstream Server 1 this booster connects to. Defaults to ``20015``.
+* ``FWBOOSTER_SERVER1ADDR``: The IP or DNS address of the Upstream Server 1 this booster connects to. Defaults to ``no.server.set``.
+* ``FWBOOSTER_SERVER1SUBSPORT``: The Subscriptions TCP port of the Upstream Server 1 that the Booster Connects to. Defaults to ``20005``.
+* ``FWBOOSTER_DELETEUNUSEDFILESETS``: Boolean value to turn Delete Unused Filesets on/off . Defaults to ``1``.
+* ``FWBOOSTER_DEBUGLEVEL``: Sets Debug Level . Defaults to ``10``.
+* ``FWBOOSTER_FILESETCHECKINTERVAL``: Sets Fileset Check interval in seconds . Defaults to ``86400``.
+* ``FWBOOSTER_BANDWITHMAXKBPS``: Sets Max Bandwidth pr Client in KBps . Defaults to ``0``.
+* ``FWBOOSTER_MAXCONNECTIONS``: Sets Max number of concurrent connections . Defaults to ``500``.
 * ``FWBOOSTER_FREEDISKSPACELIMIT``: Free Diskspace Limit. Defaults to ``500``.
 
 # Running the FileWave Booster Container
@@ -54,10 +60,10 @@ $ docker run -d --restart always --name=filewavebooster \
 
 # Specific Versions
 
-You can use the version tag to run a specific version fo the booster if needed, which you probably should do anyway (eg. 13.2.3):
+You can use the version tag to run a specific version fo the booster if needed, and you definately should in production (eg. 13.3.1):
 
 ```bash
-$ docker pull nielshojen/filewavebooster:13.2.3
+$ docker pull nielshojen/filewavebooster:13.3.1
 $ docker run -d --restart always --name=filewavebooster \
   --restart="always" \
   -p 20003:20003 \
@@ -66,5 +72,6 @@ $ docker run -d --restart always --name=filewavebooster \
   -p 20014:20014 \
   -e FWBOOSTER_NAME="supercoolbooster" \
   -e FWSERVER1_ADDRESS="myfilewaveserver.mydomain.com" \
-  nielshojen/filewavebooster:13.2.3
+  nielshojen/filewavebooster:13.3.1
 ```
+
--- run.sh
+++ run.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+/usr/bin/update-ca-trust
+
 HEXPASSWORD=$(/usr/bin/printf $FWBOOSTER_PASSWORD | /usr/bin/hexdump -C | /usr/bin/awk '{for(i=2;i<=NF;++i)print $i}' | /usr/bin/xargs | /usr/bin/sed 's/ //g' | awk -F "|" '{ print $1 }')
 
-cat fwbooster.conf | \
+cat /fwbooster.conf | \
 	sed "s/.*boosterName.*/<boosterName><ASCII><$(/bin/echo ${#FWBOOSTER_NAME})><$FWBOOSTER_NAME>/" |\
 	sed "s/.*boosterLocation.*/<boosterLocation><ASCII><$(/bin/echo ${#FWBOOSTER_LOCATION})><$FWBOOSTER_LOCATION>/" |\
-	sed "s/.*boosterPort.*/<boosterPort><UINT16><5><$FWBOOSTER_PORT>/" |\
-	sed "s/.*boosterPubPort.*/<boosterPubPort><UINT16><5><$FWBOOSTER_PUBPORT>/" |\
-	sed "s/.*server1Port.*/<server1Port><UINT16><5><$FWSERVER1_PORT>/" |\
-	sed "s/.*server1Addr.*/<server1Addr><ASCII><$(/bin/echo ${#FWSERVER1_ADDR})><$FWSERVER1_ADDR>/" |\
-	sed "s/.*useSslOnLoader.*/<useSslOnLoader><UINT16><1><$FWBOOSTER_SSLLOADER>/" |\
-	sed "s/.*server1SubsPort.*/<server1SubsPort><UINT16><5><$FWSERVER1_SUBSPORT>/" |\
+	sed "s/.*boosterPort.*/<boosterPort><UINT16><$(/bin/echo ${#FWBOOSTER_PORT})><$FWBOOSTER_PORT>/" |\
+	sed "s/.*boosterPubPort.*/<boosterPubPort><UINT16><$(/bin/echo ${#FWBOOSTER_PUBPORT})><$FWBOOSTER_PUBPORT>/" |\
+	sed "s/.*FWServerPort.*/<FWServerPort><UINT16><$(/bin/echo ${#FWBOOSTER_FWSERVERPORT})><$FWBOOSTER_FWSERVERPORT>/" |\
+	sed "s/.*FWServerAddr.*/<FWServerAddr><ASCII><$(/bin/echo ${#FWBOOSTER_FWSERVERADDR})><$FWBOOSTER_FWSERVERADDR>/" |\
+	sed "s/.*server1Port.*/<server1Port><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER1PORT})><$FWBOOSTER_SERVER1PORT>/" |\
+	sed "s/.*server1Addr.*/<server1Addr><ASCII><$(/bin/echo ${#FWBOOSTER_SERVER1ADDR})><$FWBOOSTER_SERVER1ADDR>/" |\
+	sed "s/.*server1SubsPort.*/<server1SubsPort><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER1SUBSPORT})><$FWBOOSTER_SERVER1SUBSPORT>/" |\
+	sed "s/.*server2Port.*/<server2Port><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER2PORT})><$FWBOOSTER_SERVER2PORT>/" |\
+	sed "s/.*server2Addr.*/<server2Addr><ASCII><$(/bin/echo ${#FWBOOSTER_SERVER2ADDR})><$FWBOOSTER_SERVER2ADDR>/" |\
+	sed "s/.*server2SubsPort.*/<server2SubsPort><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER2SUBSPORT})><$FWBOOSTER_SERVER2SUBSPORT>/" |\
+	sed "s/.*server3Port.*/<server3Port><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER3PORT})><$FWBOOSTER_SERVER3PORT>/" |\
+	sed "s/.*server3Addr.*/<server3Addr><ASCII><$(/bin/echo ${#FWBOOSTER_SERVER3ADDR})><$FWBOOSTER_SERVER3ADDR>/" |\
+	sed "s/.*server3SubsPort.*/<server3SubsPort><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER3SUBSPORT})><$FWBOOSTER_SERVER3SUBSPORT>/" |\
+	sed "s/.*server4Port.*/<server4Port><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER4PORT})><$FWBOOSTER_SERVER4PORT>/" |\
+	sed "s/.*server4Addr.*/<server4Addr><ASCII><$(/bin/echo ${#FWBOOSTER_SERVER4ADDR})><$FWBOOSTER_SERVER4ADDR>/" |\
+	sed "s/.*server4SubsPort.*/<server4SubsPort><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER4SUBSPORT})><$FWBOOSTER_SERVER4SUBSPORT>/" |\
+	sed "s/.*server5Port.*/<server5Port><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER5PORT})><$FWBOOSTER_SERVER5PORT>/" |\
+	sed "s/.*server5Addr.*/<server5Addr><ASCII><$(/bin/echo ${#FWBOOSTER_SERVER5ADDR})><$FWBOOSTER_SERVER5ADDR>/" |\
+	sed "s/.*server5SubsPort.*/<server5SubsPort><UINT16><$(/bin/echo ${#FWBOOSTER_SERVER5SUBSPORT})><$FWBOOSTER_SERVER5SUBSPORT>/" |\
+	sed "s/.*deleteUnusedFilesets.*/<deleteUnusedFilesets><UINT16><1><$FWBOOSTER_DELETEUNUSEDFILESETS>/" |\
+	sed "s/.*debugLevel.*/<debugLevel><UINT16><$(/bin/echo ${#FWBOOSTER_DEBUGLEVEL})><$FWBOOSTER_DEBUGLEVEL>/" |\
+	sed "s/.*filesetCheckInterval.*/<filesetCheckInterval><UINT32><$(/bin/echo ${#FWBOOSTER_FILESETCHECKINTERVAL})><$FWBOOSTER_FILESETCHECKINTERVAL>/" |\
+	sed "s/.*bandwidthMaxKBps.*/<bandwidthMaxKBps><UINT32><$(/bin/echo ${#FWBOOSTER_BANDWIDTHMAXKBPS})><$FWBOOSTER_BANDWIDTHMAXKBPS>/" |\
 	sed "s/.*password.*/<password><HEX><$(/bin/echo ${#HEXPASSWORD})><$HEXPASSWORD>/" |\
+	sed "s/.*maxConnections.*/<maxConnections><UINT16><$(/bin/echo ${#FWBOOSTER_MAXCONNECTIONS})><$FWBOOSTER_MAXCONNECTIONS>/" |\
 	sed "s/.*freeDiskSpaceLimitMB>.*/<freeDiskSpaceLimitMB><UINT64><$(/bin/echo ${#FWBOOSTER_FREEDISKSPACELIMIT})><$FWBOOSTER_FREEDISKSPACELIMIT>/" > /usr/local/etc/fwbooster.conf
 
 /etc/init.d/fw-booster start
 /usr/bin/tail -F /var/log/fwbooster.log
+
